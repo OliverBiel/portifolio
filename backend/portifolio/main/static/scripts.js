7f6e606d2dccf457b1de3668a80770e84d12c16d
OliverBiel <gmartins.oliver@gmail.com>
2022-05-20 06:59:57 -0300

    fix: Fixed menu

--- backend/portifolio/main/static/scripts.js
+++ backend/portifolio/main/static/scripts.js
@@ -1,12 +1,16 @@
 const menuElement = document.getElementById('menu')
 const menuContent = document.getElementById('menu-content')
 const iconHeader = document.querySelector('#menu-btn i')
+const header = document.getElementById("headerbox")
+
 
 function toggleMenu(){
     if (menuElement.classList.contains("closed") === true){
         menuElement.classList.remove("closed")
+        header.classList.add("closed")
     } else{
         menuElement.classList.add("closed")
+        header.classList.remove("closed")
     }
 }
 
